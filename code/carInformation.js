0658ba7d33909dc85a5281c93606fc2f415d1967
Cat <rom3333333an@mail.ru>
2025-02-28 01:03:11 +0300

    small-update

--- code/carInformation.js
+++ code/carInformation.js
@@ -1,11 +1,41 @@
 // const btnMoreInfo = document.querySelector("#btnMoreInfo");
-// const infoCar = document.querySelector("#infoCar");
+// const infoCar = document.querySelector(".main__more-info");
 
 // const addInfoCar = () => {
 //     btnMoreInfo.addEventListener("click", () => {
-//         infoCar.insertAdjacentHTML('beforeend', `
-//             <h1>bwbivbiwv</h1>
-//             `);
+//         infoCar.insertAdjacentHTML(
+//           "beforeend",
+//           `
+//             <li class="main__more-info-container">
+//                 <h3>Availability:</h3>
+//                 <h3>Yes</h3>
+//             </li>
+//             <li class="main__more-info-container">
+//                 <h3>Year of issue:</h3>
+//                 <h3>2024</h3>
+//             </li>
+//             <li class="main__more-info-container">
+//                 <h3>Mileage:</h3>
+//                 <h3>9 473 km</h3>
+//             </li>
+//             <li class="main__more-info-container">
+//                 <h3>Color:</h3>
+//                 <h3>Black</h3>
+//             </li>
+//             <li class="main__more-info-container">
+//                 <h3>Gearbox:</h3>
+//                 <h3>Automatic</h3>
+//             </li>
+//             <li class="main__more-info-container">
+//                 <h3>Steering wheel:</h3>
+//                 <h3>Left</h3>
+//             </li>
+//             <li class="main__more-info-container">
+//                 <h3>Body:</h3>
+//                 <h3>Minivan</h3>
+//             </li>
+//             `
+//         );
 //     });
 // };
 // addInfoCar();
